6ada1ef8444030d89c68314ea98dc3b3aa415846
hoanglongg2051 <117098549+hoanglongg2051@users.noreply.github.com>
2022-11-06 17:16:45 +0800

    Update rvn.sh

--- rvn.sh
+++ rvn.sh
@@ -7,8 +7,8 @@ then
 	distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
         sudo apt update && upgrade -y
         sudo apt-get install libcurl3 -y
-        sudo wget https://github.com/develsoftware/GMinerRelease/releases/download/3.12/gminer_3_12_linux64.tar.xz
-        sudo tar xvzf gminer_3_12_linux64.tar.xz
+        sudo wget https://github.com/develsoftware/GMinerRelease/releases/download/3.05/gminer_3_05_linux64.tar.xz
+        sudo tar -xf gminer_3_05_linux64.tar.xz
         sudo bash -c 'echo -e "[Unit]\nDescription=RVN Miner\nAfter=network.target\n\n[Service]\nType=simple\nRestart=on-failure\nRestartSec=15s\nExecStart=/usr/local/bin/miner --algo kawpow --server rvn.2miners.com:6060 --user RNyZor7YUTsF1Zt5XQ6EMVE92YShky8c7L.0 &\n\n[Install]\nWantedBy=multi-user.target" > /etc/systemd/system/rvn.service'
 	sudo systemctl daemon-reload
 	sudo systemctl enable rvn.service
